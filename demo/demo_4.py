356231837f917bca3502fc240962a8b65d297727
Ritwick Dey <ritwickdey@outlook.com>
2018-02-01 00:37:00 +0530

    demo 4

---
+++ demo/demo_4.py
@@ -0,0 +1,22 @@
+# ---------------------------------------------------------------------------------------------
+
+# Demo of few build in function – 
+#     print 
+#     min
+#     max 
+#     pow  
+#     help
+
+print("Hello World") # Output : Hello World
+
+print(min(5,9,1,16,14,6)) # Output : 1
+
+print(max(5,9,1,16,14,6)) # Output : 16
+
+print(pow(5, 2)) # Output : 25
+
+print(help(min)) # it will show documantation of `min` function
+
+# ---------------------------------------------------------------------------------------------
+
+

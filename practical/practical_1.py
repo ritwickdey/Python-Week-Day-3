f9a1e0dfda9a61b463ac254414d6a9f945025970
Ritwick Dey <ritwickdey@outlook.com>
2018-02-01 00:00:02 +0530

    practical 1  added

---
+++ practical/practical_1.py
@@ -0,0 +1,27 @@
+# ---------------------------------------------------------------------------------------------
+
+# 1. Create a function to calculate summation of two numbers 
+
+def sum(x, y):
+    return x+y
+
+print("Sum of 1 & 5 is", sum(1, 5)) # Output: 6
+print("Sum of 5 & 6 is", sum(5, 6)) # Output: 11
+
+# ---------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
+
+# 2. Create a ‘SayHello’ function that takes 
+# two parameter – a greeting message & your name 
+# and returns a string by concatenate your name 
+# with the getting message. 
+
+
+def SayHello(greeting, name):
+    return greeting + ", " + name + "!"
+
+
+print(SayHello("Hi", "Ritwick"))
+print(SayHello("Hello", "Akash"))
+
+# ---------------------------------------------------------------------------------------------

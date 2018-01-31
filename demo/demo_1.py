ea08c93eb6217aa7e606abf16d08f92929cb939d
Ritwick Dey <ritwickdey@outlook.com>
2018-02-01 00:25:49 +0530

    demo 1 added

---
+++ demo/demo_1.py
@@ -0,0 +1,42 @@
+# ---------------------------------------------------------------------------------------------
+
+# 1. First User Defined Function
+
+def HelloFunction():
+    return "I love Python"
+
+print(HelloFunction()) #Output: I love Python
+
+
+
+
+# ---------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
+
+# 2. 2nd function - Multiplication
+
+def Multiplication(x, y):
+    return x*y
+
+print(Multiplication(5, 7)) # output: 35
+
+
+
+
+
+# ---------------------------------------------------------------------------------------------
+# ---------------------------------------------------------------------------------------------
+
+
+# 3. '0' or '1' digit acceptor
+
+def IsSingleBinaryDigit(x):
+    return x is 0 or x is 1  
+
+
+print(IsSingleBinaryDigit(1)) # output: True
+print(IsSingleBinaryDigit(5)) # output: False
+print(IsSingleBinaryDigit(0)) # output: True
+
+
+# ---------------------------------------------------------------------------------------------

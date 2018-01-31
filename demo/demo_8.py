561a04b322fcb961aa71ec6ec92a8c14c31bff3f
Ritwick Dey <ritwickdey@outlook.com>
2018-02-01 00:57:42 +0530

    demo_8

---
+++ demo/demo_8.py
@@ -0,0 +1,7 @@
+# Demo of random module
+
+from random import *
+
+print(random()) # We'll get random float value in between 0 to 1
+print(randint(1, 25)) # We'll get random int value in between 1 to 25
+print(uniform(5, 30)) # We'll get random float value in between 5 to 30

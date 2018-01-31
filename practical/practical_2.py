24a939a77242653cb8e0facf3e2bb7608c7358a9
Ritwick Dey <ritwickdey@outlook.com>
2018-02-01 00:03:40 +0530

    practical_2 added

---
+++ practical/practical_2.py
@@ -0,0 +1,11 @@
+# ---------------------------------------------------------------------------------------------
+
+# 1. Lets modify the last task SayHello(“Hi”, “Akash”) function 
+# and make the two parameter of function as optional parameter. 
+
+def SayHello(greeting = "Hello", name = "User"):
+    return greeting + ", " + name + "!"
+
+
+print(SayHello()) # Output : Hello, User!
+print(SayHello("Hello", "Ritwick")) # Output : Hello, Ritwick!
